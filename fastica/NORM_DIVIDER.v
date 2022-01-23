67103c9a9f26aae4d754c7550754704a8de6dd11
To-ToRo <67726589+To-ToRo@users.noreply.github.com>
2022-01-23 10:25:27 +0900

    norm devider & mul1

--- fastica/NORM_DIVIDER.v
+++ fastica/NORM_DIVIDER.v
@@ -1,4 +1,4 @@
-module NORM_DIVIDER (
+module NORM_DIVIDER(
     input clk_norm,
     input en_norm,
 
@@ -11,12 +11,66 @@ module NORM_DIVIDER (
     output reg signed [25:0] w_out21, w_out22, w_out23, w_out24,
     output reg signed [25:0] w_out31, w_out32, w_out33, w_out34,
     output reg signed [25:0] w_out41, w_out42, w_out43, w_out44
-);
+    );
+
+wire signed [51:0] sum1, sum2, sum3, sum4;
+wire signed [25:0] sqrt1, sqrt2, sqrt3, sqrt4;
+wire signed [38:0] w11, w12, w13, w14,
+                   w21, w22, w23, w24,
+                   w31, w32, w33, w34,
+                   w41, w42, w43, w44;
+
+
+assign sum1 = (w_in11*w_in11 + w_in12*w_in12 + w_in13*w_in13 + w_in14*w_in14) >>> 13;
+assign sum2 = (w_in21*w_in21 + w_in22*w_in22 + w_in23*w_in23 + w_in24*w_in24) >>> 13;
+assign sum3 = (w_in31*w_in31 + w_in32*w_in32 + w_in33*w_in33 + w_in34*w_in34) >>> 13;
+assign sum4 = (w_in41*w_in41 + w_in42*w_in42 + w_in43*w_in43 + w_in44*w_in44) >>> 13;
+
+assign w11 = w_in11;
+assign w12 = w_in12;
+assign w13 = w_in13;
+assign w14 = w_in14;
+assign w21 = w_in21;
+assign w22 = w_in22;
+assign w23 = w_in23;
+assign w24 = w_in24;
+assign w31 = w_in31;
+assign w32 = w_in32;
+assign w33 = w_in33;
+assign w34 = w_in34;
+assign w41 = w_in41;
+assign w42 = w_in42;
+assign w43 = w_in43;
+assign w44 = w_in44;
+
 
 always @(posedge clk_norm) begin
-    if (en_norm) begin
-        
-    end
+    if (en_norm) begin 
+        w_out11 <= (w11 <<< 13) / sqrt1; w_out12 <= (w12 <<< 13) / sqrt1; w_out13 <= (w13 <<< 13) / sqrt1; w_out14 <= (w14 <<< 13) / sqrt1;
+        w_out21 <= (w21 <<< 13) / sqrt2; w_out22 <= (w22 <<< 13) / sqrt2; w_out23 <= (w23 <<< 13) / sqrt2; w_out24 <= (w24 <<< 13) / sqrt2;
+        w_out31 <= (w31 <<< 13) / sqrt3; w_out32 <= (w32 <<< 13) / sqrt3; w_out33 <= (w33 <<< 13) / sqrt3; w_out34 <= (w34 <<< 13) / sqrt3;
+        w_out41 <= (w41 <<< 13) / sqrt4; w_out42 <= (w42 <<< 13) / sqrt4; w_out43 <= (w43 <<< 13) / sqrt4; w_out44 <= (w44 <<< 13) / sqrt4;    
+    end 
 end
-    
+
+SQRT2 SQRT1 (
+    .x(sum1),
+    .y(sqrt1)
+);
+
+SQRT2 SQRT2 (
+    .x(sum2),
+    .y(sqrt2)
+);
+
+SQRT2 SQRT3 (
+    .x(sum3),
+    .y(sqrt3)
+);
+
+SQRT2 SQRT4 (
+    .x(sum4),
+    .y(sqrt4)
+);
+
 endmodule
--- fastica/one_unit_mul1.v
+++ fastica/one_unit_mul1.v
@@ -2,7 +2,7 @@
  * MUL 1
  * zw1(4x4) = z(4x1)*w1(1x4) zw2, zw3, zw4
  */
-module ONE_UNIT_MUL1 (
+module MUL1 (
     input clk_mul,
     input en_mul,
     
@@ -133,71 +133,71 @@ begin
     if (en_mul) begin
         // active
         zw1_11_reg <= z1 * w11;
-        zw1_12_reg <= z1 * w12;
-        zw1_13_reg <= z1 * w13;
-        zw1_14_reg <= z1 * w14;
-        zw1_21_reg <= z2 * w11;
+        zw1_12_reg <= z2 * w11;
+        zw1_13_reg <= z3 * w11;
+        zw1_14_reg <= z4 * w11;
+        zw1_21_reg <= z1 * w12;
         zw1_22_reg <= z2 * w12;
-        zw1_23_reg <= z2 * w13;
-        zw1_24_reg <= z2 * w14;
-        zw1_31_reg <= z3 * w11;
-        zw1_32_reg <= z3 * w12;
+        zw1_23_reg <= z3 * w12;
+        zw1_24_reg <= z4 * w12;
+        zw1_31_reg <= z1 * w13;
+        zw1_32_reg <= z2 * w13;
         zw1_33_reg <= z3 * w13;
-        zw1_34_reg <= z3 * w14;
-        zw1_41_reg <= z4 * w11;
-        zw1_42_reg <= z4 * w12;
-        zw1_43_reg <= z4 * w13;
+        zw1_34_reg <= z4 * w13;
+        zw1_41_reg <= z1 * w14;
+        zw1_42_reg <= z2 * w14;
+        zw1_43_reg <= z3 * w14;
         zw1_44_reg <= z4 * w14;
 
         zw2_11_reg <= z1 * w21;
-        zw2_12_reg <= z1 * w22;
-        zw2_13_reg <= z1 * w23;
-        zw2_14_reg <= z1 * w24;
-        zw2_21_reg <= z2 * w21;
+        zw2_12_reg <= z2 * w21;
+        zw2_13_reg <= z3 * w21;
+        zw2_14_reg <= z4 * w21;
+        zw2_21_reg <= z1 * w22;
         zw2_22_reg <= z2 * w22;
-        zw2_23_reg <= z2 * w23;
-        zw2_24_reg <= z2 * w24;
-        zw2_31_reg <= z3 * w21;
-        zw2_32_reg <= z3 * w22;
+        zw2_23_reg <= z3 * w22;
+        zw2_24_reg <= z4 * w22;
+        zw2_31_reg <= z1 * w23;
+        zw2_32_reg <= z2 * w23;
         zw2_33_reg <= z3 * w23;
-        zw2_34_reg <= z3 * w24;
-        zw2_41_reg <= z4 * w21;
-        zw2_42_reg <= z4 * w22;
-        zw2_43_reg <= z4 * w23;
+        zw2_34_reg <= z4 * w23;
+        zw2_41_reg <= z1 * w24;
+        zw2_42_reg <= z2 * w24;
+        zw2_43_reg <= z3 * w24;
         zw2_44_reg <= z4 * w24;
 
         zw3_11_reg <= z1 * w31;
-        zw3_12_reg <= z1 * w32;
-        zw3_13_reg <= z1 * w33;
-        zw3_14_reg <= z1 * w34;
-        zw3_21_reg <= z2 * w31;
+        zw3_12_reg <= z2 * w31;
+        zw3_13_reg <= z3 * w31;
+        zw3_14_reg <= z4 * w31;
+        zw3_21_reg <= z1 * w32;
         zw3_22_reg <= z2 * w32;
-        zw3_23_reg <= z2 * w33;
-        zw3_24_reg <= z2 * w34;
-        zw3_31_reg <= z3 * w31;
-        zw3_32_reg <= z3 * w32;
+        zw3_23_reg <= z3 * w32;
+        zw3_24_reg <= z4 * w32;
+        zw3_31_reg <= z1 * w33;
+        zw3_32_reg <= z2 * w33;
         zw3_33_reg <= z3 * w33;
-        zw3_34_reg <= z3 * w34;
-        zw3_41_reg <= z4 * w31;
-        zw3_42_reg <= z4 * w32;
-        zw3_43_reg <= z4 * w33;
+        zw3_34_reg <= z4 * w33;
+        zw3_41_reg <= z1 * w34;
+        zw3_42_reg <= z2 * w34;
+        zw3_43_reg <= z3 * w34;
         zw3_44_reg <= z4 * w34;
 
         zw4_11_reg <= z1 * w41;
-        zw4_12_reg <= z1 * w42;
-        zw4_13_reg <= z1 * w43;
-        zw4_14_reg <= z1 * w44;
-        zw4_21_reg <= z2 * w41;
+        zw4_12_reg <= z2 * w41;
+        zw4_13_reg <= z3 * w41;
+        zw4_14_reg <= z4 * w41;
+        zw4_21_reg <= z1 * w42;
         zw4_22_reg <= z2 * w42;
-        zw4_23_reg <= z2 * w43;
-        zw4_24_reg <= z2 * w44;
-        zw4_31_reg <= z3 * w41;
-        zw4_32_reg <= z3 * w42;
+        zw4_23_reg <= z3 * w42;
+        zw4_24_reg <= z4 * w42;
+        zw4_31_reg <= z1 * w43;
+        zw4_32_reg <= z2 * w43;
         zw4_33_reg <= z3 * w43;
-        zw4_34_reg <= z3 * w44;
-        zw4_41_reg <= z4 * w41;
-        zw4_42_reg <= z4 * w42;
-        zw4_43_reg <= z4 * w43;
+        zw4_34_reg <= z4 * w43;
+        zw4_41_reg <= z1 * w44;
+        zw4_42_reg <= z2 * w44;
+        zw4_43_reg <= z3 * w44;
         zw4_44_reg <= z4 * w44;
     end else begin
         zw1_11_reg <= w11;
